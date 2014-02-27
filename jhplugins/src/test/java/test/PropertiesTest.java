18ee2488b1772a4b90fa9a7f2fe82d7145fe261d
huzefapoonawala <poonawalahuzefa@gmail.com>
2014-02-27 11:59:27 +0530

    env var bug fix

--- jhplugins/src/test/java/test/PropertiesTest.java
+++ jhplugins/src/test/java/test/PropertiesTest.java
@@ -8,24 +8,26 @@ import javax.annotation.Resource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.jh.util.CommonUtil;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:jh-main-context.xml","classpath:jh-properties-context.xml"}) 
+@ContextConfiguration(locations={"classpath:jh-main-context.xml"})
+@ActiveProfiles(value={"development","nowebsite"})
 public class PropertiesTest {
 	
-	@Value("${jhplugins.path.edi.orgill}") private String orgillEdiPath;
-	@Value("${jhplugins.path.properties}") private String path;
+	@Value("${jhplugins.path.remote.edi.orgill}") private String orgillEdiPath;
+	@Value("#{systemEnvironment['jhplugins.path.properties'] ?: 'NA.properties'}") private String path;
 	
 	@Resource private CommonUtil commonUtil;
 
 	@Test
 	public void testExternal() throws Exception {
 		System.out.println(path);
-		commonUtil.createFile(orgillEdiPath, "test.txt", "This is test\nedi format test".getBytes());
+//		commonUtil.createFile(orgillEdiPath, "test.txt", "This is test\nedi format test".getBytes());
 		System.out.println(orgillEdiPath);
 	}
 }
