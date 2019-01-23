1d4b7f44cee858b919dc01ab8321def4985734ff
HUZEFA <HUZEFA@DESKTOP-B1E5FTU>
2019-01-23 12:06:51 +0530

    committing changes

--- jhplugins-etl/src/main/java/com/jh/etl/EtlApplication.java
+++ jhplugins-etl/src/main/java/com/jh/etl/EtlApplication.java
@@ -1,14 +1,33 @@
 package com.jh.etl;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.jh.etl.common.interfaces.EtlOrchestrator;
 
 @SpringBootApplication
 public class EtlApplication {
 
+	private final Log LOG = LogFactory.getLog(EtlApplication.class);
+	
+	@Autowired(required=false) private Optional<List<EtlOrchestrator>> orchestrators;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(EtlApplication.class, args);
+		SpringApplication.run(EtlApplication.class, args).getBean(EtlApplication.class).initiateEtlOrchestration();
+	}
+	
+	public void initiateEtlOrchestration(){
+		LOG.debug("Start: Initiating orchestration");
+		
+		orchestrators.ifPresent((orchs) -> orchs.forEach(orch -> orch.initiateEtl()));
+		
+		LOG.debug("End: Initiating orchestration");
 	}
 
 }
-
---
+++ jhplugins-etl/src/main/java/com/jh/etl/common/interfaces/DataExtracter.java
@@ -0,0 +1,6 @@
+package com.jh.etl.common.interfaces;
+
+public interface DataExtracter<T> {
+
+	T extractData();
+}
